291ca0f18f8bc2dd4d585b6d6f8e85f529b6b1de
Atibrewa <atibrewa@macalester.edu>
2023-12-01 10:47:34 -0600

    reference for using fonts

--- harmoniser/app/page.tsx
+++ harmoniser/app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { verdana } from './ui/fonts';
 
 export default function Page() {
   return (
@@ -10,7 +11,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-colours-2 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${verdana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             Welcome to Harmoniser. A place to sleep, relax, focus, and let all of your stress melt away.
           </p>
           <div className="flex gap-6">
