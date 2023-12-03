ba9900de0500e2add5f363dc95475500a9013c5e
Nina Lin <77794473+nlin02@users.noreply.github.com>
2023-12-03 17:10:59 -0600

    Adding video player (not exactly working right now)

--- harmoniser/app/ui/dashboard/render-visualiser.tsx
+++ harmoniser/app/ui/dashboard/render-visualiser.tsx
@@ -1,3 +1,14 @@
+import React from 'react'
+
+// import perfect_night_dance from '../../public/visualizers/perfect_night_dance.mp4'
+
 export default function Visualiser(name: string) {
-    return <p>Name!, {name}</p>;
+  
+    return (
+      <div className="container">
+        <video width="640" height="320" controls> 
+          <source src= '../../public/visualizers/perfect_night_dance.mp4' type="video/mp4"/>
+        </video>
+      </div>
+    );
   }
--- harmoniser/next.config.js
+++ harmoniser/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config, options) {
+        config.module.rules.push({
+          test: /\.mp3$/,
+          use: {
+            loader: "file-loader",
+          },
+        });
+        return config;
+      },
+};
 
 module.exports = nextConfig;
