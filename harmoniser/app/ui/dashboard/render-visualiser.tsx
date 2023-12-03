33e2db5dc311ab753926e9c88fc9b553828dfbc7
Atibrewa <atibrewa@macalester.edu>
2023-12-03 16:18:05 -0600

    template page for nina!

---
+++ harmoniser/app/ui/dashboard/render-visualiser.tsx
@@ -0,0 +1,3 @@
+export default function Visualiser(name: string) {
+    return <p>Name!, {name}</p>;
+  }
--- harmoniser/app/ui/dashboard/topnav.tsx
+++ harmoniser/app/ui/dashboard/topnav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
+import Logo from '@/app/ui/logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function TopNav() {
