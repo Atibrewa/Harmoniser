90f4e824d7612fa53c32c6c3ca55c004c502767b
Atibrewa <atibrewa@macalester.edu>
2023-12-03 19:05:01 -0600

    sidenav fix

--- harmoniser/app/ui/dashboard/sidenav.tsx
+++ harmoniser/app/ui/dashboard/sidenav.tsx
@@ -15,7 +15,7 @@ export default function SideNav() {
           <Logo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow flex-col justify-between space-y-2 md:flex-col md:space-x-0 md:space-y-2">
         <VisualizerLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-colours-2 md:block"></div>
       </div>
