fc5a10a3dc05335d36f9fadb46e470fbbd0924c3
Atibrewa <atibrewa@macalester.edu>
2023-12-03 18:58:47 -0600

    page switch w/o refreshing now :)

--- harmoniser/app/ui/dashboard/nav-links.tsx
+++ harmoniser/app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,7 @@ import {
   QuestionMarkCircleIcon,
   CogIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,14 +23,14 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className="flex !mt-0 h-[48px] grow items-center justify-center gap-2 rounded-md bg-colours-2 p-3 text-sm font-medium hover:bg-colours-1 hover:text-colours-5 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
