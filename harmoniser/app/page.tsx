cfedac22d207c4ec89c926c5ecea026c1f3b0a30
Atibrewa <atibrewa@macalester.edu>
2023-12-03 18:12:45 -0600

    images for landing page!

--- harmoniser/app/page.tsx
+++ harmoniser/app/page.tsx
@@ -2,6 +2,7 @@ import Logo from '@/app/ui/logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { verdana, verdana_bold } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -36,7 +37,20 @@ export default function Page() {
           </div>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image 
+            src="/landing_desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="A graphic of a woman listening to music ina yoga pose, desktop version"
+          />
+          <Image 
+            src="/landing_mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="A graphic of a woman listening to music ina yoga pose, mobile version"
+          />
         </div>
       </div>
     </main>
