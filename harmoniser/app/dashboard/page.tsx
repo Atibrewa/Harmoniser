afd0a7d25b1c3a706de4d428d46bb9f118f8fb9a
Atibrewa <atibrewa@macalester.edu>
2023-12-06 11:31:37 -0600

    help and settings pages!

--- harmoniser/app/dashboard/page.tsx
+++ harmoniser/app/dashboard/page.tsx
@@ -3,4 +3,9 @@ import Visualiser from "../ui/dashboard/render-visualiser";
 export default function Page() {
     const tempvis = Visualiser('hello');
     return tempvis;
+
+    // return <div className={`rounded-lg bg-colours-2 p-6`}>
+    // <h1 className={`${comfortaa.className} text-2xl md:text-3xl`}>FAQs</h1><br></br>
+    
+    // </div>
   }
