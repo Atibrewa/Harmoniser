82a2f8e2c68dd41aa128c173bb4621bd404d255b
Atibrewa <atibrewa@macalester.edu>
2023-12-06 17:13:40 -0600

    working on pages!

--- harmoniser/app/dashboard/page.tsx
+++ harmoniser/app/dashboard/page.tsx
@@ -2,10 +2,10 @@ import Visualiser from "../ui/dashboard/render-visualiser";
 import { comfortaa } from "../ui/fonts";
 
 export default function Page() {
-    const tempvis = Visualiser('hello');
+  const tempvis = Visualiser('hello');
 
-    return <div className={`rounded-lg bg-colours-2 p-6`}>
-    <h1 className={`${comfortaa.className} text-2xl md:text-3xl`}>Harmoniser</h1><br></br>
-      {tempvis}
-    </div>
-  }
+  return <div className={`rounded-lg bg-colours-2 p-6`}>
+  <h1 className={`${comfortaa.className} text-2xl md:text-3xl`}>Harmoniser</h1><br></br>
+    {tempvis}
+  </div>
+}
