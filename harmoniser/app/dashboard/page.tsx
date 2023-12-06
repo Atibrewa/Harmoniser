3197b9b4033cc1622a2115fd4c3ce9cd62629663
Atibrewa <atibrewa@macalester.edu>
2023-12-06 11:33:15 -0600

    formatting to prep for changes :)

--- harmoniser/app/dashboard/page.tsx
+++ harmoniser/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import Visualiser from "../ui/dashboard/render-visualiser";
+import { comfortaa } from "../ui/fonts";
 
 export default function Page() {
     const tempvis = Visualiser('hello');
-    return tempvis;
 
-    // return <div className={`rounded-lg bg-colours-2 p-6`}>
-    // <h1 className={`${comfortaa.className} text-2xl md:text-3xl`}>FAQs</h1><br></br>
-    
-    // </div>
+    return <div className={`rounded-lg bg-colours-2 p-6`}>
+    <h1 className={`${comfortaa.className} text-2xl md:text-3xl`}>Harmoniser</h1><br></br>
+      {tempvis}
+    </div>
   }
