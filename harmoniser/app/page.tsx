36ae6e7fc55ccb771fc67dc5ddbb70ae459e244e
Atibrewa <atibrewa@macalester.edu>
2023-12-12 17:28:01 -0600

    logo files differentiation

--- harmoniser/app/page.tsx
+++ harmoniser/app/page.tsx
@@ -1,4 +1,4 @@
-import Logo from '@/app/ui/logo';
+import Logo from '@/app/ui/landing-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { verdana, verdana_bold } from './ui/fonts';
