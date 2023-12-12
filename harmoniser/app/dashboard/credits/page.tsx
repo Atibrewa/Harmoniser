b85898f49fc71640eb17cd861e5267840c08652c
Atibrewa <atibrewa@macalester.edu>
2023-12-12 17:22:27 -0600

    added compressed visualisers and CLEANUP

--- harmoniser/app/dashboard/credits/page.tsx
+++ harmoniser/app/dashboard/credits/page.tsx
@@ -6,6 +6,8 @@ export default function Page() {
 
   return <div className={`rounded-lg bg-colours-2 p-6`}>
     <h1 className={`${comfortaa.className} text-2xl md:text-3xl`}>Credits</h1><br></br>
+    <p className={p}>The audio and video we have used come from many different artists who we appreciate a lot for their work and for making it free to use! These are links to their work, we hope you check them out :)</p>
+
   </div>
 
 }
--- harmoniser/app/ui/dashboard/topnav.tsx
+++ harmoniser/app/ui/dashboard/topnav.tsx
@@ -40,7 +40,7 @@ export default function TopNav() {
     <div className="flex h-full flex-row px-3 py-4 md:px-2">
       <div className="flex grow flex-row justify-between gap-2 space-x-2 md:flex-row md:space-x-0 md:space-y-2">
         <div className="flex h-[48px] grow items-center justify-center gap-2 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 rounded-md bg-colours-1 w-32 text-white md:w-40">
-          <p>Welcome, Username.</p>
+          <p>Welcome, Listener.</p>
         </div>
         <NavLinks />
         <div className="!mt-0 hidden h-[48px] w-full rounded-md bg-colours-2 md:block"></div>
