0b02207c5b486cdbc957cc9881c7166834b93f1b
Atibrewa <atibrewa@macalester.edu>
2023-12-17 04:15:29 -0600

    improved reactive sizes for mobile

--- harmoniser/app/page.tsx
+++ harmoniser/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-colours-2 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${verdana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${verdana.className} text-xl text-gray-800 lg:text-3xl lg:leading-normal`}>
             <span className={`${verdana_bold.className}`}>Welcome to Harmoniser.</span> A place to sleep, relax, focus, and let all of your stress melt away.
           </p>
           <div className="flex gap-6">
@@ -20,18 +20,18 @@ export default function Page() {
               <h1 className = {`${verdana.className} text-xl`}>Returning User?</h1>
               <Link
                 href="/login"
-                className="flex items-center gap-5 self-start rounded-lg bg-colours-5 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-colours-4 md:text-base"
+                className="flex items-center gap-5 self-start rounded-lg bg-colours-5 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-colours-4 lg:text-base"
               >
-              <span>Log In</span> <ArrowRightIcon className="w-5 md:w-6" />
+              <span>Log In</span> <ArrowRightIcon className="w-5 lg:w-6" />
               </Link>
             </div>
             <div className="flex flex-col justify-center gap-6">
               <h1 className = {`${verdana.className} text-xl`}>New Here?</h1>
               <Link
                 href="/signup"
-                className="flex items-center gap-5 self-start rounded-lg bg-colours-5 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-colours-4 md:text-base"
+                className="flex items-center gap-5 self-start rounded-lg bg-colours-5 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-colours-4 lg:text-base"
               >
-                <span>Sign Up</span> <ArrowRightIcon className="w-5 md:w-6" />
+                <span>Sign Up</span> <ArrowRightIcon className="w-5 lg:w-6" />
               </Link>
             </div>
           </div>
@@ -41,14 +41,14 @@ export default function Page() {
             src="/landing_desktop.png"
             width={1000}
             height={760}
-            className="hidden md:block"
+            className="hidden lg:block"
             alt="A graphic of a woman listening to music in a yoga pose, desktop version"
           />
           <Image 
             src="/landing_mobile.png"
             width={560}
             height={620}
-            className="block md:hidden"
+            className="block lg:hidden"
             alt="A graphic of a woman listening to music in a yoga pose, mobile version"
           />
         </div>
